d3e65731fff992431a36eb94a3031eec028f4db1
rahzagul <paras.mehra@gmail.com>
2015-10-16 14:59:18 -0400

    Update ec2-hostname.sh

--- ec2-hostname.sh
+++ ec2-hostname.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 
-echo $USER
-sudo -s <<EOF1
-echo Now I am root
-echo $USER
-
 USER_DATA=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/public-hostname`
 HOSTNAME=`echo $USER_DATA`
 IPV4=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
-# IPV4=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
 
-  # Set the host name
-  hostname $HOSTNAME
-  echo $HOSTNAME > /etc/hostname
+sudo -s <<EOF
+rm /etc/hosts
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "$IPV4 $HOSTNAME" >> /etc/hosts
+EOF
 
-  # Add fqdn to hosts file
-  sudo cat<<EOF2 > /etc/hosts
-  # This file is automatically genreated by ec2-hostname script
-  127.0.0.1 localhost
-  $IPV4 $HOSTNAME
-  EOF2
+sudo -s <<EOF
+rm /etc/sysconfig/network
+echo "NETWORKING=yes" >> /etc/sysconfig/network
+echo "NETWORKING_IPV6=no" >> /etc/sysconfig/network
+echo "HOSTNAME=$USER_DATA" >> /etc/sysconfig/network
+EOF
 
-  # Add fqdn to network file
-  sudo cat<<EOF3 > /etc/sysconfig/network
-  NETWORKING=yes
-  NETWORKING_IPV6=no
-  HOSTNAME=$USER_DATA
-  EOF3
-
-  # Disable SELinux
-  sudo cat<<EOF4 > /etc/selinux/config
-  # This file controls the state of SELinux on the system.
-  # SELINUX= can take one of these three values:
-  #     enforcing - SELinux security policy is enforced.
-  #     permissive - SELinux prints warnings instead of enforcing.
-  #     disabled - No SELinux policy is loaded.
-  SELINUX=disabled
-  # SELINUXTYPE= can take one of these two values:
-  #     targeted - Targeted processes are protected,
-  #     mls - Multi Level Security protection.
-  SELINUXTYPE=targeted
-  EOF4
-EOF1
+sudo -s <<EOF
+rm /etc/selinux/config
+echo "# This file controls the state of SELinux on the system." >> /etc/selinux/config
+echo "# SELINUX= can take one of these three values:" >> /etc/selinux/config
+echo "#     enforcing - SELinux security policy is enforced." >> /etc/selinux/config
+echo "#     permissive - SELinux prints warnings instead of enforcing." >> /etc/selinux/config
+echo "#     disabled - No SELinux policy is loaded." >> /etc/selinux/config
+echo "SELINUX=disabled" >> /etc/selinux/config
+echo "# SELINUXTYPE= can take one of these two values:" >> /etc/selinux/config
+echo "#     targeted - Targeted processes are protected," >> /etc/selinux/config
+echo "#     mls - Multi Level Security protection." >> /etc/selinux/config
+echo "SELINUXTYPE=targeted" >> /etc/selinux/config
+EOF
